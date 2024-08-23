27de03f75185b773cb7088f94fa90917acdb4615
thuthuy490 <99247527+thuthuy490@users.noreply.github.com>
2024-08-24 01:34:13 +0700

    SQL - Exploratory Data Analysis.sql

--- SQL - Exploratory Data Analysis.sql
+++ SQL - Exploratory Data Analysis.sql
@@ -4,15 +4,79 @@
  
 -- --------------------------------------------------------------------------------------------------------------
 
-/*What are the most common sentiments expressed by time*/
+/*What are the most common sentiments expressed by Dealer*/
 SELECT 
-    *
-FROM
-    ig_clone.users
-ORDER BY created_at asc
-LIMIT 10;
+   d.Dealer_Name,
+   s.Sentiment,
+   count(*) as Dealer_Sen
+FROM Dealers d JOIN
+Sentiment s ON d.Zip_Code = s.Postal_Code
+GROUP BY d.Dealer_Name, s.Sentiment
+ORDER BY count(*);
 -- --------------------------------------------------------------------------------------------------------------
 
-/*Are there specific car models or features that frequently lead to negative sentiment*/
-How does customer sentiment correlate with safety recalls or other product issues?
-Can we identify patterns in customer sentiment over time (e.g., seasonal trends, changes after product updates)?
+/*Are there specific Dealer that frequently lead to negative sentiment by Year*/
+with Count_Sentiment as
+(
+SELECT 
+   Year,
+   Postal_Code,
+   count(*) as CountSentiment,
+   DENSE_RANK() OVER (PARTITION BY Year ORDER BY count(*) Desc, Postal_Code Desc) as rank
+FROM
+    Sentiment
+WHERE
+	Sentiment='Negative'
+GROUP BY
+	Year,
+	Postal_Code
+)
+SELECT 
+   d.Dealer_Name,
+   c.Year,
+   c.CountSentiment
+FROM Dealers d JOIN
+Count_Sentiment c ON d.Zip_Code = c.Postal_Code
+wHERE rank = 1
+ORDER BY Year;
+-- --------------------------------------------------------------------------------------------------------------
+
+/*Can we identify patterns in dealer sentiment over time (e.g., seasonal trends)?*/
+DECLARE 
+    @columns NVARCHAR(MAX) = '', 
+    @sql     NVARCHAR(MAX) = '';
+ 
+-- select the category names
+SELECT 
+    @columns+=QUOTENAME(Dealer_Name) + ','
+FROM 
+   [Dealers]
+ORDER BY 
+    Dealer_Name;
+ 
+-- remove the last comma
+SET @columns = LEFT(@columns, LEN(@columns) - 1);
+ 
+-- construct dynamic SQL
+SET @sql ='
+SELECT * FROM   
+(
+   SELECT
+         d.Dealer_Name DealerName ,
+         month(s.Date) Month ,
+		 s.Year,
+		 count (*) Sales
+  FROM [Dealers] d
+  RIGHT OUTER JOIN [Sentiment] s on d.Zip_Code = s.Postal_Code
+  AND s.Sentiment= ''Negative''
+  GROUP BY d.Dealer_Name, month(s.Date), s.Year
+
+) 
+t 
+PIVOT(
+    sum (Sales) for  DealerName in ('+ @columns +')
+) AS pivot_table;';
+ 
+-- execute the dynamic SQL
+EXECUTE sp_executesql @sql;
+-- --------------------------------------------------------------------------------------------------------------
